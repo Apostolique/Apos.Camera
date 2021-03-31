851184d1a48169d143e42787185b6e08ca6c7b78
Jean-David Moisan <vynecompiler@gmail.com>
2021-03-30 23:17:26 -0400

    Fix typo

--- Source/DefaultViewport.cs
+++ Source/DefaultViewport.cs
@@ -2,7 +2,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Apos.Camera {
-    public class DefaulViewport : SplitViewport {
-        public DefaulViewport(GraphicsDevice graphicsDevice, GameWindow window) : base(graphicsDevice, window, 0f, 0f, 1f, 1f) { }
+    public class DefaultViewport : SplitViewport {
+        public DefaultViewport(GraphicsDevice graphicsDevice, GameWindow window) : base(graphicsDevice, window, 0f, 0f, 1f, 1f) { }
     }
 }
